e082badb57a21d9101595f99f5e2be462852672c
cholponklv <cbaiel@mail.ru>
2024-08-29 14:17:27 +0600

    settings.py

--- dastbek/settings.py
+++ dastbek/settings.py
@@ -147,6 +147,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
